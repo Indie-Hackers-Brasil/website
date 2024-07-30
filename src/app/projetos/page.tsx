3859e5528572ebf319c829a8154f036a7590da16
João Inácio Neto <birobirobiro.dev@gmail.com>
2024-07-30 20:19:33 +0000

    Add first projects

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,8 +1,20 @@
+import projects from '../../data/projects';
+
 export default function Page() {
   return (
     <main className="flex flex-col gap-2 text-balance">
       <h1 className="text-2xl font-bold">Projetos</h1>
       <p>Conheça os projetos de produtos digitais brasileiros.</p>
+      <div className="space-y-4 py-4">
+        {projects.map((project, index) => (
+          <a href={project.site} className="mt-2 inline-block" target="_blank" key={index}>
+            <div className="rounded border p-4 shadow-sm transition-transform ease-in-out hover:scale-105 hover:border-violet-500 hover:shadow-md">
+              <h2 className="text-xl font-semibold">{project.titulo}</h2>
+              <p>{project.descricao}</p>
+            </div>
+          </a>
+        ))}
+      </div>
     </main>
   );
 }
---
+++ src/data/projects.ts
@@ -0,0 +1,59 @@
+const projects = [
+  {
+    "titulo": "Alertpix",
+    "site": "https://alertpix.live/",
+    "descricao": "Alertpix é uma plataforma que permite streamers receberem no pix com a menor taxa de todas trazendo voz de I.A e segurança aos usuários, com uma comunidade incrível."
+  },
+  {
+    "titulo": "Lo Cafe",
+    "site": "https://lo.cafe/",
+    "descricao": "Lo Cafe, feito pelo clã Uchira, é amplamente popular e promovido por influenciadores gringos."
+  },
+  {
+    "titulo": "Poku",
+    "site": "https://poku.io/",
+    "descricao": "Poku é um test runner open-source que facilita e agiliza os testes em Node.js, Bun, Deno, criado por @weslley_io."
+  },
+  {
+    "titulo": "Loja na Bio",
+    "site": "https://www.lojanabio.com/",
+    "descricao": "Loja na Bio ajuda criadores de conteúdo a venderem seus produtos com um catálogo virtual e recebimento de pedidos diretamente no WhatsApp."
+  },
+  {
+    "titulo": "BlindPay",
+    "site": "https://blindpay.com/",
+    "descricao": "BlindPay revoluciona a forma de transacionar criptomoedas, sendo a primeira plataforma web3 desta lista, criada por @bebedaselva."
+  },
+  {
+    "titulo": "Menthor",
+    "site": "https://menthor.io/",
+    "descricao": "Menthor oferece cursos gratuitos para aprender sobre web3 e outras tecnologias, criada por @bebedaselva."
+  },
+  {
+    "titulo": "Css2Wind",
+    "site": "https://www.css2wind.com/",
+    "descricao": "Css2Wind é um minigame divertido para aprender Tailwind CSS, desenvolvido por @LukeberryPi."
+  },
+  {
+    "titulo": "Sales Whisper",
+    "site": "https://saleswhisper.com.br/",
+    "descricao": "Sales Whisper ajuda com vendas utilizando o WhatsApp como ferramenta, liderado por @pedro_cto."
+  },
+  {
+    "titulo": "Codante",
+    "site": "http://codante.io/",
+    "descricao": "Codante oferece cursos e workshops de programação para aprimorar suas habilidades, com conteúdos atualizados."
+  },
+  {
+    "titulo": "Comanda Rápida",
+    "site": "https://comandarapida.com.br/",
+    "descricao": "Comanda Rápida é um sistema de gestão empresarial com planos a partir de R$ 7,90, desenvolvido por @velfo e @andreluis2."
+  },
+  {
+    "titulo": "HumanTrack",
+    "site": "https://humantrack.io/",
+    "descricao": "HumanTrack está construindo a próxima geração em monitoramento de saúde, melhorando a experiência tanto para profissionais de saúde quanto para pacientes."
+  }
+];
+
+export default projects;

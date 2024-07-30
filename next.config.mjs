7e146a4f7332d2fea166bb7824e23ccee2bc57f3
Rychillie <rychillie@rychillie.net>
2024-07-30 14:25:09 -0300

    setup Cloudflare Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
 
-export default nextConfig;
+// note: the if statement is present because you
+//       only need to use the function during development
+if (process.env.NODE_ENV === 'development') {
+	await setupDevPlatform()
+}
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+export default nextConfig

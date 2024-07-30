739ba28ef1227b391233e3e351aa4c766a117c6b
Rychillie <rychillie@rychillie.net>
2024-07-30 15:54:56 -0300

    Improve Links Style

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,24 @@
+'use client'
+
+import c from 'clsx';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import type { ReactNode } from 'react';
 
 type ItemMenuProps = {
   children: ReactNode;
   href: string;
+  active?: boolean;
 };
 
-function ItemMenu({ children, href }: ItemMenuProps) {
+function ItemMenu({ children, href, active }: ItemMenuProps) {
   return (
     <Link
       href={href}
-      className="relative flex px-2 py-1 align-middle transition-all hover:text-neutral-800/60 dark:hover:text-neutral-200/60"
+      className={c(
+        "relative flex px-2 py-1 align-middle decoration-neutral-900 underline-offset-4 transition-all hover:text-neutral-800/60 hover:underline dark:decoration-neutral-100 dark:hover:text-neutral-200/60",
+        active && 'underline'
+      )}
     >
       {children}
     </Link>
@@ -18,6 +26,8 @@ function ItemMenu({ children, href }: ItemMenuProps) {
 }
 
 export default function NavBar() {
+  const pathname = usePathname()
+
   const menuItems = [
     { label: 'criadores', href: '/criadores' },
     { label: 'projetos', href: '/projetos' },
@@ -32,11 +42,15 @@ export default function NavBar() {
       </Link>
 
       <div className="-mx-2 flex">
-        {menuItems.map((item, index) => (
-          <ItemMenu key={index} href={item.href}>
-            {item.label}
-          </ItemMenu>
-        ))}
+        {menuItems.map((item, index) => {
+          const active = pathname === item.href;
+
+          return (
+            <ItemMenu key={index} href={item.href} active={active}>
+              {item.label}
+            </ItemMenu>
+          )
+        })}
       </div>
     </nav>
   );

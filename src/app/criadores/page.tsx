f365c4e31a9fcd3ee8e99b8e60494b732ba25e00
Matheus Trindade <trindadematheus27@gmail.com>
2024-07-31 14:05:22 -0300

    changing text description color and add external link icon

--- src/app/criadores/page.tsx
+++ src/app/criadores/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <main className="flex flex-col gap-2 text-balance">
       <h1 className="text-2xl font-bold">Criadores</h1>
-      <p>Conheça os criadores de produtos digitais brasileiros.</p>
+      <p className='text-neutral-400'>Conheça os criadores de produtos digitais brasileiros.</p>
     </main>
   );
 }

94899fc3d25fd063a9a165f869607eaf927a6eeb
João Inácio Neto <birobirobiro.dev@gmail.com>
2024-07-31 11:56:20 -0300

    Adding the first projects (temporary) (#3)
    * Add first projects
    
    * Update home phrases and link to projects page
    
    * Adjust Poku's phrase
    
    * chore: Update tsconfig.json and layout.tsx, improve some pages, and update navbar.tsx
    
    ---------
    
    Co-authored-by: Weslley Araújo <46850407+wellwelwel@users.noreply.github.com>
    Co-authored-by: Rychillie <rychillie@rychillie.net>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <main className="flex flex-1 flex-col items-center justify-center gap-2 text-balance text-center">
+    <main className="flex flex-1 flex-col items-center justify-center gap-4 text-balance text-center">
       <h1 className="text-3xl font-bold">Em desenvolvimento ⚠️</h1>
-      <p>Estamos trabalhando para trazer o melhor conteúdo para você.</p>
+      <p>Estamos dedicados a trazer o melhor conteúdo para você. Fique ligado(a)!</p>
+      <p>
+        <i>
+          Enquanto isso, confira os{' '}
+          <Link href="/projetos" className="text-blue-500 underline hover:text-blue-700">
+            projetos
+          </Link>{' '}
+          que já estão disponíveis.
+        </i>
+      </p>
     </main>
   );
 }
--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,8 +1,20 @@
+import projects from '@/data/projects';
+
 export default function Page() {
   return (
-    <main className="flex flex-col gap-2 text-balance">
+    <main className="flex flex-col gap-2">
       <h1 className="text-2xl font-bold">Projetos</h1>
       <p>Conheça os projetos de produtos digitais brasileiros.</p>
+      <div className="space-y-4 py-4">
+        {projects.map((project, index) => (
+          <a href={project.site} className="mt-2 inline-block" target="_blank" key={index}>
+            <div className="flex flex-col gap-2 rounded-lg border border-neutral-200 p-4 shadow-sm transition-all hover:scale-105 hover:border-violet-500 hover:shadow-md dark:border-neutral-800">
+              <h2 className="text-xl font-semibold">{project.titulo}</h2>
+              <p className="text-sm">{project.descricao}</p>
+            </div>
+          </a>
+        ))}
+      </div>
     </main>
   );
 }
